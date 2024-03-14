983b94d9a4cc71ffc2567e6457e92d6a89792f70
Felipe Cavalcanti Andrade de Araújo <jpfelipec@academicos.com>
2024-03-13 21:28:11 -0300

    final aula 02

--- aula02/src/App.jsx
+++ aula02/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import RenderizandoComFuncoes from './components/RenderizandoComFuncoes'
+import TrabalhandoComImagens from './components/TrabalhandoComImagens'
+import HookContador from './components/HookContador'
+
 
 const App = () => {
   return (
     <div>
-      <RenderizandoComFuncoes />
+      <HookContador/>
     </div>
   )
 }
---
+++ aula02/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import reactLogo from '../assets/reactLogo.png'
+
+
+
+const TrabalhandoComImagens = () => {
+  return (
+    <div>
+        <img src='playwrightLogo.png' alt=""/>
+        <img src={reactLogo} alt=""/>
+
+    </div>
+  )
+}
+
+export default TrabalhandoComImagens
