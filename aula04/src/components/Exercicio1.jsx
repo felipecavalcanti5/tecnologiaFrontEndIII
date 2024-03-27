b24f08d9dfe4d124b880af0dd4ecf010072c32f6
Felipe Cavalcanti Andrade de Araújo <jpfelipec@academicos.com>
2024-03-27 20:49:20 -0300

    aula 04 adicionada

---
+++ aula04/src/components/Exercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1 = () => {
+  return (
+    <div>
+        <p>Teste de CSS</p>
+        <img src="https://cdn.britannica.com/71/118771-050-C1CFADBA/Dromedary-rider.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Exercicio1
